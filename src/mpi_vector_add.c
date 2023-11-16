3e7e44bad082f0e0aad0a653a2def63ac393682b
Julian Camacho <jjulian.341@gmail.com>
2023-11-15 18:56:52 -0600

    Added mpi_vector_add.c comments

--- src/mpi_vector_add.c
+++ src/mpi_vector_add.c
@@ -3,34 +3,39 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    int pid, np;
-    int elements_per_process;
-    int vector_size;
-    MPI_Status status;
+    // Definiciones generales
+    int pid, np;                // Identificador y número total de procesos
+    int elements_per_process;   // Número de elementos por proceso
+    int vector_size;            // Tamaño total del vector
+    MPI_Status status;          // Estructura para almacenar el estado de la operación MPI
 
+    // Inicialización de MPI
     MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &pid);
-    MPI_Comm_size(MPI_COMM_WORLD, &np);
+    MPI_Comm_rank(MPI_COMM_WORLD, &pid); // Obtener el rango (identificador) del proceso actual
+    MPI_Comm_size(MPI_COMM_WORLD, &np);  // Obtener el número total de procesos
 
+    // Manejo de argumentos de línea de comandos
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <vector_size>\n", argv[0]);
-        MPI_Abort(MPI_COMM_WORLD, 1);
+        MPI_Abort(MPI_COMM_WORLD, 1);   // Abortar MPI si no se proporciona el tamaño del vector
     }
-
+    
+    // Asignación del tamaño del vector y cálculo de elementos por proceso
     vector_size = atoi(argv[1]);
     elements_per_process = vector_size / np;
 
+    // Asignación de memoria para los vectores
     int *a = (int *)malloc(vector_size * sizeof(int));
     int *b = (int *)malloc(vector_size * sizeof(int));
     int *c = (int *)malloc(vector_size * sizeof(int));
 
-    // Inicializar los vectores a y b (puedes cambiar esto según tus necesidades)
+    // Inicializar los vectores a y b
     for (int i = 0; i < vector_size; i++) {
         a[i] = vector_size - 1;
         b[i] = vector_size + 1;
     }
 
-     // Get the name of the processor
+    // Obtener el nombre del procesador
     char processor_name[MPI_MAX_PROCESSOR_NAME];
     int name_len;
     MPI_Get_processor_name(processor_name, &name_len);
@@ -38,6 +43,17 @@ int main(int argc, char *argv[]) {
            pid, processor_name, pid, np);
 
     // Distribuir partes de los vectores a los otros procesos 
+    /* 
+     * MPI Scatter toma los siguientes argumentos en este orden:
+     *   - Dirección inicial del buffer de salida
+     *   - Número de elementos que se envía a cada proceso
+     *   - Tipo de dato que se va a enviar
+     *   - Direción del buffer de recepción
+     *   - Número de elementos que espera recibir cada proceso
+     *   - Tipo de datos de los elementos a recibir 
+     *   - Rango (rank) del proceso raíz (el que realizará el envío)
+     *   - Comunicador por el que realizar la comunicación.
+     */
     MPI_Scatter(a, elements_per_process, MPI_INT, a, elements_per_process, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Scatter(b, elements_per_process, MPI_INT, b, elements_per_process, MPI_INT, 0, MPI_COMM_WORLD);
 
@@ -52,6 +68,9 @@ int main(int argc, char *argv[]) {
     }
 
     // Recolectar los resultados parciales en el proceso raíz
+    /*
+     * MPI_Gather toma los mismos argumentos que MPI_Scatter, pero para recibir los datos 
+     */
     MPI_Gather(c, elements_per_process, MPI_INT, c, elements_per_process, MPI_INT, 0, MPI_COMM_WORLD);
 
     // Medir el tiempo de finalización
@@ -90,10 +109,12 @@ int main(int argc, char *argv[]) {
         fclose(fp);
     }
 
+    // Liberar memoria
     free(a);
     free(b);
     free(c);
 
+    // Finalizar MPI
     MPI_Finalize();
 
     return 0;
